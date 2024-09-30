7d6761f189942cd9ddad4f73fc3679beb5ae1149
giorgiCS <sarishviligiorgi80@gmail.com>
2024-09-30 23:55:27 +0400

    fix

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_URL, //
+  process.env.SUPABASE_KEY // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4cGNibnJjY3luYnp5dXVmcHp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1MjMxNDgsImV4cCI6MjAzODA5OTE0OH0.QMuXfmMl3SEz31Rpsfl49DnlvQxdlL9J49fo9ooCP5g
 );
